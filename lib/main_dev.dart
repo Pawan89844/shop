1dba4bbb072cd6419a738b39099de3b615108589
Pawan Kumar <pk89844@gmail.com>
2024-07-11 14:36:10 +0530

    Container error

---
+++ lib/main_dev.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+import 'shop.dart';
+
+void main() => runApp(const Shop());
--- lib/module/profile/view/profile_view.dart
+++ lib/module/profile/view/profile_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:shop/constants/app_colors.dart';
 import 'package:shop/constants/app_string.dart';
 import 'package:shop/widgets/app_bold_text.dart';
