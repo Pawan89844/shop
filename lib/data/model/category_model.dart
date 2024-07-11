31988a6aeb42e5c789fdbd7dbc6f5ce7841276e2
Pawan Kumar <pk89844@gmail.com>
2024-07-11 15:15:29 +0530

    Dummy Data Added

---
+++ lib/data/model/category_model.dart
@@ -0,0 +1,8 @@
+class CategoriesModel {
+  final int id;
+  final String title;
+  final String featuredImage;
+
+  CategoriesModel(
+      {required this.id, required this.title, required this.featuredImage});
+}
