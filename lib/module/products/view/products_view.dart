7e776c2970b05d184b86ef993f7ae5091381d0ad
Pawan Kumar <pk89844@gmail.com>
2024-07-25 13:02:16 +0530

    Pages linked

--- lib/module/products/view/products_view.dart
+++ lib/module/products/view/products_view.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/constants/app_string.dart';
 import 'package:shop/data/dummy/dummy_product_details.dart';
 import 'package:shop/data/dummy/dummy_products.dart';
 import 'package:shop/module/home/view/components/products_grid.dart';
+import 'package:shop/routes/app_state.dart';
+import 'package:shop/routes/navigate_page.dart';
+import 'package:shop/widgets/app_bold_text.dart';
+import 'package:shop/widgets/app_text.dart';
 
 import '../../../data/model/products_model.dart';
 
@@ -11,8 +17,17 @@ class ProductsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final appstate = Provider.of<AppState>(context);
+    final actions = [
+      IconButton(
+          onPressed: () => NavigateTo(appstate).searchPage(),
+          icon: const Icon(Icons.search))
+    ];
     return Scaffold(
-      appBar: AppBar(forceMaterialTransparency: true),
+      appBar: AppBar(
+          forceMaterialTransparency: true,
+          actions: actions,
+          title: AppText(AppString.productsPage.toUpperCase(), fontSize: 18.0)),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
